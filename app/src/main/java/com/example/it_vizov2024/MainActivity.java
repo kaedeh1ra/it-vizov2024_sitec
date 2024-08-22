4e61c8c2df36f4c4d1068e1f9f5ea971990511c3
Денис <denyar3100@gmail.com>
2024-08-22 20:52:47 +0400

    откат

--- app/src/main/java/com/example/it_vizov2024/MainActivity.java
+++ app/src/main/java/com/example/it_vizov2024/MainActivity.java
@@ -18,10 +18,6 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
