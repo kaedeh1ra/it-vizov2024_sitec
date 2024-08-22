8185de4fac8f5e7229741b861e10910e9c5aa11d
Денис <denyar3100@gmail.com>
2024-08-22 17:32:47 +0400

    почему у меня имя денис((

--- app/src/main/java/com/example/it_vizov2024/MainActivity.java
+++ app/src/main/java/com/example/it_vizov2024/MainActivity.java
@@ -7,13 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.example.it_vizov2024.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
 
     }
