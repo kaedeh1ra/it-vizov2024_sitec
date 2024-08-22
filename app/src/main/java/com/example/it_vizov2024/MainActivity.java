58c72591a57cede0af62143c35e72310564d32f2
Денис <denyar3100@gmail.com>
2024-08-22 17:34:52 +0400

    а ща?

--- app/src/main/java/com/example/it_vizov2024/MainActivity.java
+++ app/src/main/java/com/example/it_vizov2024/MainActivity.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 import com.example.it_vizov2024.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
